6785a5ecd6e7bd79bdbd22d981c4e575f78b2bfe
Robert Zurer <robert@zurer.com>
2012-12-01 08:29:35 -0500

    removed common files

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 /*jslint browser: true*/
 /*global  window, localStorage, $*/
 "use strict";
-var common = require('./modules/common').common(),
-	eventListener = require('./modules/eventListener').eventListener(),
+var common = require('../common/modules/common').common(),
+	eventListener = require('../common/modules/eventListener').eventListener(),
 	initializeNavigator = function () {
 		window.navigationControl = require('./modules/navigationControl').navigationControl(eventListener, common);
 	},
